081a71c0ba3ed300147b6b0475e821f62e6449e0
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-02-03 22:53:25 +0200

    ForLoopExercise04

---
+++ src/ForLoopExercise04/_01_NumbersEndingIn7.java
@@ -0,0 +1,16 @@
+package ForLoopExercise04;
+
+
+public class _01_NumbersEndingIn7 {
+
+    public static void main(String[] args) {
+
+        for (int number = 7; number <= 997; number += 10) {
+
+            System.out.println(number);
+
+
+        }
+    }
+
+}
---
+++ src/ForLoopExercise04/_04_CleverLily.java
@@ -0,0 +1,40 @@
+package ForLoopExercise04;
+
+import java.util.Scanner;
+
+public class _04_CleverLily {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = Integer.parseInt(scanner.nextLine());
+        double washingMachinePrice = Double.parseDouble(scanner.nextLine());
+        int toyPrice = Integer.parseInt(scanner.nextLine());
+
+        int toys = 0;
+        double lilyMoney = 0.00;
+        double birthdayMoney = 10.00;
+
+        for (int i = 1; i <= age; i++) {
+
+            if (i % 2 == 0) {
+
+                lilyMoney += birthdayMoney;
+                birthdayMoney += 10.00;
+                lilyMoney -= 1.00;
+            } else {
+
+                toys++;
+            }
+        }
+
+         lilyMoney += toys * toyPrice;
+
+        if (lilyMoney >= washingMachinePrice) {
+            System.out.printf("Yes! %.2f", lilyMoney - washingMachinePrice);
+        } else {
+            System.out.printf("No! %.2f", washingMachinePrice - lilyMoney);
+        }
+    }
+}
