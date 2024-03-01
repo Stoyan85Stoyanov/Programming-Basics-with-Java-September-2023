bbdcb0ab06c82c20b551cbda827b58eeaf8b2754
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-01 23:06:59 +0200

    NestedLoopsMoreExercises07

---
+++ src/NestedLoopsMoreExercises07/_13_PrimePairs.java
@@ -0,0 +1,48 @@
+package NestedLoopsMoreExercises07;
+
+import java.util.Scanner;
+
+public class _13_PrimePairs {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        int d = Integer.parseInt(scanner.nextLine());
+        int maxA = a + c;
+        int maxB = b + d;
+        int counterA;
+        int counterB;
+
+        for (int i = a; i <= maxA; i++) {
+            counterA = 0;
+
+            for (int k = 1; k <= i; k++) {
+                if (i % k == 0) {
+
+                    counterA++;
+                }
+            }
+            if (counterA == 2) {
+
+                for (int j = b; j <= maxB; j++) {
+                    counterB = 0;
+
+                    for (int l = 1; l <= j; l++) {
+                        if (j % l == 0) {
+
+                            counterB++;
+                        }
+                    }
+                    if (counterB == 2) {
+
+                        System.out.printf("%d%d", i, j);
+                        System.out.println();
+                    }
+                }
+            }
+        }
+    }
+}
